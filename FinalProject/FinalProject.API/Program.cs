fa272cbd6940ba9a83152ae58993aac87db0acd6
Kiarikita <isilzulalguner@gmail.com>
2022-07-06 23:08:36 +0300

    Database settings has been done.

--- FinalProject/FinalProject.API/Program.cs
+++ FinalProject/FinalProject.API/Program.cs
@@ -1,11 +1,32 @@
+using FinalProject.Core.Repositories;
+using FinalProject.Core.UnitOfWork;
+using FinalProject.Repository;
+using FinalProject.Repository.Repositories;
+using FinalProject.Repository.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
 
 var app = builder.Build();
 
